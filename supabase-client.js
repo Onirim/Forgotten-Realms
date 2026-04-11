82e8ad55ffcee6e66e049f8ff3cfd2c05e69b70c
Thibaut C. <20838877+Onirim@users.noreply.github.com>
2026-04-11 14:58:42 +0200

    Initial commit

---
+++ supabase-client.js
@@ -0,0 +1,4 @@
+// ── Config Supabase partagée ──────────────────────────────────
+const SUPABASE_URL = 'https://dhimaaiqlkizndbhzsdt.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_9x0KcnXGdwgb9J7mWnZ05g_8Jtm4zT-';
+const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
